80c781952a8b75c15ea1ed1d112328b2734a7be1
hiep692k-ux <hiep692k@gmail.com>
2026-02-07 15:53:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ with col2:
                 try:
                     # Gọi Google Gemini
                     genai.configure(api_key=api_key)
-                    model = genai.GenerativeModel('gemini-1.5-flash-latest')
+                    model = genai.GenerativeModel('gemini-1.5-pro')
                     
                     # Ghép Prompt hệ thống + Dữ liệu khách hàng
                     full_request = [system_prompt]
